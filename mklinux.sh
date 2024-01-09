225a4735e22ff8316eead21944f705a779b76d4b
aliriza <aliriza.keskin@pardus.org.tr>
2024-01-09 11:44:30 +0000

    fix fs module

--- mklinux.sh
+++ mklinux.sh
@@ -209,7 +209,7 @@ if [[ "${no_build}" == "" ]] ; then
     ./scripts/config --enable CONFIG_LOGO_LINUX_CLUT224
 
     # embed all filesystem modules
-    grep "^CONFIG_[A-Z]*_FS=m" .config  | cut -f1 -d"=" | while read cfg ; do
+    grep "^CONFIG_[A-Z0-9]*_FS=m" .config  | cut -f1 -d"=" | while read cfg ; do
         ./scripts/config --enable $cfg
     done
 
